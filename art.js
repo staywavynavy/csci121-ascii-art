771b694fdcc4f361105787843cde176968997079
JadenLews <119870564+JadenLews@users.noreply.github.com>
2023-04-27 15:05:41 -0400

    added sonic url info

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+   {"title": "Sonic",
+   "filename": "Sonic.txt",
+   "credit_text": "Jaden Lewis",
+   "credit_url": "https://www.asciiart.eu/video-games/sonic-the-hedgehog"},
 ];
