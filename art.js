db739aba90ffa1415766874f18aad3697492a03f
MFogarty16 <119897384+MFogarty16@users.noreply.github.com>
2023-04-27 15:10:24 -0400

    Update art.js
    Add cactus details to art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Cactus",
+   "filename": "cactus.txt",
+   "credit_text": "kra",
+   "credit_url": "https://www.asciiart.eu/plants/cactus"},
 ];
