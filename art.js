02071db4177bb6c4818b8c0a9a0df423af1e894f
Emilyeganderson <119871032+Emilyeganderson@users.noreply.github.com>
2023-04-27 15:13:24 -0400

    Add frogs details to art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Frogs",
+   "filename": "frogs.txt",
+   "credit_text": "VK",
+   "credit_url": "https://www.asciiart.eu/animals/frogs"},
 ];
