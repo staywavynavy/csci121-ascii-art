ceb1025baecc32283e32ecdb1967faf46a08a28e
WillSetlik <119877618+WillSetlik@users.noreply.github.com>
2023-04-27 15:04:44 -0400

    Add Capitals logo details

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Capitals Logo",
+   "filename": "capitals.txt",
+   "credit_text": "Brian Odom",
+   "credit_url": "https://www.asciiart.eu/sports-and-outdoors/ice-hockey"},
 ];
